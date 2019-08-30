0d94356297ce0624b93226d4e1fa7ffee5129aff
Dan Begallie <dan_begallie@hotmail.com>
2019-08-29 21:40:28 -0400

    Updated colors for Amy.  Changed Alt to Non-School.  Added evening Non-School times

--- sleep-clock.py
+++ sleep-clock.py
@@ -6,9 +6,13 @@ import unicornhat as unicorn
 import colorconstants
 
 early_warning = colorconstants.colors["goldenrod1"]
-late_warning = colorconstants.colors["orange"]
+#! AMY late_warning = purple3
+#! JAMES late_warning = orange
+late_warning = colorconstants.colors["purple3"]
 sleeping = colorconstants.colors["red1"]
-awake = colorconstants.colors["green1"]
+#! AMY awake = deeppink1
+#! JAMES awake = green1
+awake = colorconstants.colors["deeppink1"]
 awake_warning = colorconstants.colors["blue"]
 normal = colorconstants.colors["black"]
 current_color = normal
@@ -16,17 +20,20 @@ current_color = normal
 while True:
     now_date = datetime.datetime.today()
     school_out = datetime.date(datetime.datetime.today().year, 6, 15)
-    school_in = datetime.date(datetime.datetime.today().year, 9, 1)
+    school_in = datetime.date(datetime.datetime.today().year, 8, 29)
     current_date = datetime.date(now_date.year, now_date.month, now_date.day)
-    use_alt = False
+    use_non_school_mornings = False
     if current_date.weekday() == 5 or current_date.weekday() == 6 or (school_out < current_date < school_in):
-        use_alt = True
-    early_warning_start = datetime.time(19, 30)
-    late_warning_start = datetime.time(19, 50)
-    sleeping_start = datetime.time(20, 0)
-    awake_start = datetime.time(6, 30) if use_alt == False else datetime.time(8, 00)
-    awake_warning_start = datetime.time(7, 15) if use_alt == False else datetime.time(8, 45)
-    awake_normal_start = datetime.time(8, 00) if use_alt == False else datetime.time(9, 00)
+        use_non_school_mornings = True
+    use_non_school_evenings = False
+    if current_date.weekday() == 4 or current_date.weekday() == 5 or (school_out < current_date < school_in):
+        use_non_school_evenings = True
+    early_warning_start = datetime.time(19, 30) if use_non_school_evenings == False else datetime.time(20, 00)
+    late_warning_start = datetime.time(19, 50) if use_non_school_evenings == False else datetime.time(20, 20)
+    sleeping_start = datetime.time(20, 00) if use_non_school_evenings == False else datetime.time(20, 30)
+    awake_start = datetime.time(6, 45) if use_non_school_mornings == False else datetime.time(8, 00)
+    awake_warning_start = datetime.time(7, 30) if use_non_school_mornings == False else datetime.time(8, 45)
+    awake_normal_start = datetime.time(8, 00) if use_non_school_mornings == False else datetime.time(9, 00)
     new_color = current_color
     now_time = datetime.datetime.now()
     current_time = datetime.time(now_time.hour, now_time.minute)
