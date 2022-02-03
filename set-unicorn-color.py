3ce9e94decf19ba3181a60f88e453c35830b13c5
Dan Begallie <dan@siegeltech.io>
2022-02-03 01:44:30 -0500

    Update for new times

--- set-unicorn-color.py
+++ set-unicorn-color.py
@@ -7,41 +7,41 @@ import getopt
 import colorconstants
 
 red_value = 0
-green_value  = 255
+green_value = 255
 blue_value = 0
 
 try:
-  options, args = getopt.getopt(sys.argv[1:], 'hr:g:b:c:x:', ['red=', 'green=', 'blue=', 'color=', 'hex='])
+    options, args = getopt.getopt(sys.argv[1:], 'hr:g:b:c:x:', ['red=', 'green=', 'blue=', 'color=', 'hex='])
 except getopt.GetoptError:
-  print('set-unicorn-color.py -r <red value> -g <green value> -b <blue value>')
-  print('set-unicorn-color.py -c <defined color name>')
-  print('set-unicorn-color.py -x <color hex code>')
-  sys.exit(2)
-
-for option, arg in options:
-  if option == '-h':
     print('set-unicorn-color.py -r <red value> -g <green value> -b <blue value>')
     print('set-unicorn-color.py -c <defined color name>')
     print('set-unicorn-color.py -x <color hex code>')
-    sys.exit()
-  elif option in ("-r", "--red"):
-     red_value = arg
-  elif option in ("-g", "--green"):
-     green_value = arg
-  elif option in ("-b", "--blue"):
-     blue_value = arg
-  elif option in ("-c", "--color"):
-     red_value, green_value, blue_value = colorconstants.colors[arg]
+    sys.exit(2)
+
+for option, arg in options:
+    if option == '-h':
+        print('set-unicorn-color.py -r <red value> -g <green value> -b <blue value>')
+        print('set-unicorn-color.py -c <defined color name>')
+        print('set-unicorn-color.py -x <color hex code>')
+        sys.exit()
+    elif option in ("-r", "--red"):
+        red_value = arg
+    elif option in ("-g", "--green"):
+        green_value = arg
+    elif option in ("-b", "--blue"):
+        blue_value = arg
+    elif option in ("-c", "--color"):
+        red_value, green_value, blue_value = colorconstants.colors[arg]
 
 
 unicorn.set_layout(unicorn.AUTO)
 unicorn.rotation(0)
 unicorn.brightness(1)
-width,height=unicorn.get_shape()
+width, height = unicorn.get_shape()
 
 for y in range(height):
-  for x in range(width):
-    unicorn.set_pixel(x, y, red_value, green_value, blue_value)
+    for x in range(width):
+        unicorn.set_pixel(x, y, red_value, green_value, blue_value)
 
 unicorn.show()
 
--- sleep-clock.py
+++ sleep-clock.py
@@ -6,12 +6,12 @@ import unicornhat as unicorn
 import colorconstants
 
 early_warning = colorconstants.colors["goldenrod1"]
-#! AMY late_warning = purple3
-#! JAMES late_warning = orange
+# AMY late_warning = purple3
+# JAMES late_warning = orange
 late_warning = colorconstants.colors["purple3"]
 sleeping = colorconstants.colors["red1"]
-#! AMY awake = deeppink1
-#! JAMES awake = green1
+# AMY awake = deeppink1
+# JAMES awake = green1
 awake = colorconstants.colors["deeppink1"]
 awake_warning = colorconstants.colors["blue"]
 normal = colorconstants.colors["black"]
@@ -28,12 +28,12 @@ while True:
     use_non_school_evenings = False
     if current_date.weekday() == 4 or current_date.weekday() == 5 or (school_out < current_date < school_in):
         use_non_school_evenings = True
-    early_warning_start = datetime.time(19, 30) if use_non_school_evenings == False else datetime.time(20, 00)
-    late_warning_start = datetime.time(19, 50) if use_non_school_evenings == False else datetime.time(20, 20)
-    sleeping_start = datetime.time(20, 00) if use_non_school_evenings == False else datetime.time(20, 30)
-    awake_start = datetime.time(6, 45) if use_non_school_mornings == False else datetime.time(8, 00)
-    awake_warning_start = datetime.time(7, 30) if use_non_school_mornings == False else datetime.time(8, 45)
-    awake_normal_start = datetime.time(8, 00) if use_non_school_mornings == False else datetime.time(9, 00)
+    early_warning_start = datetime.time(19, 30) if use_non_school_evenings is False else datetime.time(20, 00)
+    late_warning_start = datetime.time(19, 50) if use_non_school_evenings is False else datetime.time(20, 20)
+    sleeping_start = datetime.time(20, 00) if use_non_school_evenings is False else datetime.time(20, 30)
+    awake_start = datetime.time(6, 30) if use_non_school_mornings is False else datetime.time(8, 00)
+    awake_warning_start = datetime.time(7, 40) if use_non_school_mornings is False else datetime.time(8, 45)
+    awake_normal_start = datetime.time(8, 00) if use_non_school_mornings is False else datetime.time(9, 00)
     new_color = current_color
     now_time = datetime.datetime.now()
     current_time = datetime.time(now_time.hour, now_time.minute)
@@ -53,11 +53,11 @@ while True:
         unicorn.set_layout(unicorn.AUTO)
         unicorn.rotation(0)
         unicorn.brightness(0.4 if new_color == sleeping else 1)
-        width,height=unicorn.get_shape()
+        width, height = unicorn.get_shape()
         red_value, green_value, blue_value = new_color
         for y in range(height):
-          for x in range(width):
-            unicorn.set_pixel(x, y, red_value, green_value, blue_value)
+            for x in range(width):
+                unicorn.set_pixel(x, y, red_value, green_value, blue_value)
         current_color = new_color
         unicorn.show()
     time.sleep(1)
